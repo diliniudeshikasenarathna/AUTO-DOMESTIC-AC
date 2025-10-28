e7d8514691e4f42ffcf3702c9b22fd8cfae4f9a0
diliniudeshikasenarathna <dilinikariyawasam.kbdu@gmail.com>
2025-10-28 17:13:21 +0530

    jwt

--- src/main/java/com/rash/be/entity/User.java
+++ src/main/java/com/rash/be/entity/User.java
@@ -5,14 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Data
@@ -24,27 +22,24 @@ public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String username;
     private String password;
     private String email;
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
-    @Override
-   public String getPassword(){
-        return password;
-    }
-
-    @Override
-    public String getUsername(){
-        return username;
-    }
 
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        return List.of();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
     }
 
     @Override
